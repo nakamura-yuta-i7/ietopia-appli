26197c2a8a6c916a9a0df2cdc4dbd07ce2c3a93b
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2017-02-12 16:49:35 +0900

    lineat

--- www/dist/bundle.js
+++ www/dist/bundle.js
@@ -690,7 +690,7 @@ class Dispatcher {
 // 家とぴあAPI:基点URL
 module.exports = {
   API_BASE_URL:  false ? "http://0.0.0.0:8888" : "http://0.0.0.0:8888",
-  IETOPIA_LINE_AT_URL: "https://page.line.me/faw4681t",
+  IETOPIA_LINE_AT_URL: "https://line.me/R/ti/p/%40faw4681t",
   IETOPIA_GOOGLE_MAP_URL: "https://goo.gl/maps/xjzHWazSb1S2",
   IETOPIA_PRIVACY_POLICY_URL: "http://www.ietopia.jp/pages/privacy?smp=1",
   IETOPIA_GAIYO_URL: "http://www.ietopia.jp/companies?smp=1",
