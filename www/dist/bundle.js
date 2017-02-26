6a28fe81273eb1057ae1b0c2c624e121aff0963f
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2017-02-26 15:42:08 +0900

    npm run build-for-prod

--- www/dist/bundle.js
+++ www/dist/bundle.js
@@ -21280,7 +21280,7 @@ class Dispatcher {
 
 // 家とぴあAPI:基点URL
 module.exports = {
-  API_BASE_URL:  false ? "https://appli.ietopia-services.com" : "http://0.0.0.0:8888",
+  API_BASE_URL:  true ? "https://appli.ietopia-services.com" : "http://0.0.0.0:8888",
   IETOPIA_LINE_AT_URL: "https://line.me/R/ti/p/%40faw4681t",
   IETOPIA_GOOGLE_MAP_URL: "https://goo.gl/maps/xjzHWazSb1S2",
   IETOPIA_PRIVACY_POLICY_URL: "http://www.ietopia.jp/pages/privacy?smp=1",
@@ -23855,7 +23855,7 @@ function onDeviceReady() {
   })
   .then(()=>{
     console.log( "global.APP", global.APP );
-    console.log( "IS_PRODUCTION", false );
+    console.log( "IS_PRODUCTION", true );
     global.renderPage();
   })
   .catch((err)=>{
