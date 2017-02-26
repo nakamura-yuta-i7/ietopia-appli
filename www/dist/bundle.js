ec5eff29654b3fb9dd8568698a0b0cc3ec4f0d86
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2017-02-26 16:19:02 +0900

    build-for-prod

--- www/dist/bundle.js
+++ www/dist/bundle.js
@@ -4716,7 +4716,10 @@ class SearchHistoryApi extends IetopiaMeApiBase {
     this.setApiUrlSufix("/search_history");
   }
   get() {
-    return this.request().then( data => JSON.parse(data.params_json) );
+    return this.request().then( data => {
+      if ( data.params_json ) return JSON.parse(data.params_json);
+      return {};
+    } );
   }
   save(params_json="{}") {
     var url = this.url + "/save";
